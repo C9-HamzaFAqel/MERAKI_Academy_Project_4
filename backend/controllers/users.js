b6af72ac02e18e108dda2ef24b2219ee6e5ddb3f
HamzaFAqel <hamzah.f.aqel@gmail.com>
2024-01-05 20:50:47 +0300

    back end/creat function (register)

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -1,4 +1,23 @@
-const UserModel=
+const usersModel=require("../models/users")
 const register=(req,res)=>{
-
+const {firstName,lastName, email,password,age, country, grade ,role }=req.body
+const newUser= new usersModel({
+    firstName,lastName, email,password,age, country, grade ,role
+})
+newUser.save().then((result)=>{
+    res.status(201).json({
+        success: true,
+        message: "Account Created Successfully",
+        user: result
+    })
+}).catch((err)=>{
+    {err.keyPattern? res.status(409).json({
+        success: false,
+        message: `The email already exists`,
+      }) : res.status(500).json({
+        success: false,
+        message: `Server Error`,
+        err: err.message,
+      })}
+})
 }
